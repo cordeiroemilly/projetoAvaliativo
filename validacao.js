5c01075f2a1045765773dd317d15873d38daec56
EMILLY DA SILVA CORDEIRO <cordeiro.emilly@alunoescolassociais.org.br>
2021-11-24 16:20:01 -0300

    adicionando arquivo de javascript

--- validacao.js
+++ validacao.js
@@ -10,3 +10,28 @@ function verificar(){
          alert("campos preenchidos com sucesso");
      }
 }
+
+
+function maskfone(){
+	var numeroAtual = document.getElementsById('fone').value;
+	const isCelular = numeroAtual.length === 11;
+	const isFone = numeroAtual.length === 10;
+	let numeroAjustado;
+	if(isCelular){
+		const part1 = numeroAtual.slice(0,2);
+        const part2 = numeroAtual.slice(2,7);
+        const part3 = numeroAtual.slice(7,11);
+        numeroAjustado = `(${part1}) ${part2}-${part3}`
+    } else if(isFone){
+    	const part1 = numeroAtual.slice(0,2);
+        const part2 = numeroAtual.slice(2,6);
+        const part3 = numeroAtual.slice(6,10);
+        numeroAjustado =`(${part1}) ${part2}-${part3}`
+    } else{
+    	numeroAtual.value = '';
+    	alert("Número informado é incorreto, favor verificar!")
+    }
+    numeroAtual.value = numeroAjustado;
+}
+
+
